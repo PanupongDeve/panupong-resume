c494cc39b4933f1e4a360300497da515dee989ed
panupongdeve <panupong.dev.web@gmail.com>
2022-07-03 17:08:40 +0000

    [ADD] - lock mobile

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Navbar from './components/Navbar'
@@ -6,7 +6,27 @@ import Wrapper from './components/Wrapper'
 import  ResumeCard from './components/ResumeCard'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const handleResize = () => {
+    
+    setWidth(window.innerWidth)
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  if (width <= 1200) {
+    return (
+      <div className="layout notfound">
+        <h1>Disply is not represent at less width 1200 px and not support mobile. Thank !</h1>
+      </div>
+    )
+  }
+
+  console.log('width', width)
 
   return (
     <div className="layout">
