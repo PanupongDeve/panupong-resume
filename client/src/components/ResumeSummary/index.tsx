7db131246d54ce1d2f8a8998f03cdc0af89d647f
Panupong Chamsomboon <panupong.dev.web@gmail.com>
2022-07-03 21:19:37 +0000

    [ADD] text in about

--- client/src/components/ResumeSummary/index.tsx
+++ client/src/components/ResumeSummary/index.tsx
@@ -6,11 +6,20 @@ const  ResumeSummary = () => {
     return (
         <CardSection className="summary-section" title="About">
             <div className='resume-summary-content'>
-                <p className="mb-0">
+                <p className="mb-5">
                         &nbsp;&nbsp;&nbsp;3 years Software Engineer, interesting  Software Development,  
                         UI/UX, Infrastructure Cloud management and keep learning about knowleage technology technical 
                         for help company make profit.
                 </p>
+                <div className='resume-summary-code'>
+                    <pre>
+                        {
+                        `while(!dead) { 
+    code();
+};`    
+                        }
+                    </pre>
+                </div>
             </div>
         </CardSection>
     )
