186dab646c0abf4b0f92980e0ab2b4cf5bf80f30
Panupong Chamsomboon <panupong.dev.web@gmail.com>
2022-10-02 14:30:06 +0000

    remove image

--- client/src/components/CardHeaderWithImage/index.tsx
+++ client/src/components/CardHeaderWithImage/index.tsx
@@ -6,7 +6,7 @@ const CardHeaderWithImage = ({ img, children }: any) => {
         <header className="header-card">
                 <div className="row">
                     <div className="col-block col-md-auto resume-picture-holder text-center text-md-start">
-                        <img className='picture picture-profile' src={img} alt="" />
+                        {/* <img className='picture picture-profile' src={img} alt="" /> */}
                     </div>
                     {
                         children
